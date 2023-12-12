82e4fc793988463d21ceee3527fc7a0bf2ddb17b
Laktoso <151091748+Laktoso@users.noreply.github.com>
2023-12-11 23:13:12 -0500

    Add files via upload

---
+++ alex.css
@@ -0,0 +1,72 @@
+body {
+  font-family: Arial;
+  background-image: url("Fondo.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+  
+  header {
+    background-color: rgb(16, 16, 75);
+    color: gray;
+    padding: 25px;
+    text-align: center;  
+  }
+  
+  header h1 {
+    margin: 0;  
+  }
+  
+  header nav {
+    display: inline-block;
+    text-align: left;  
+  }
+  
+  header nav ul {
+    list-style: none;
+    margin: 2;
+    padding: 2;  
+  }
+  
+  header nav li {
+    display: inline-block;
+    margin-right: 20px;  
+  }
+  
+  a {
+    color: rgb(207, 30, 30);
+    text-decoration: none;  
+  }
+  
+  #logo {
+    width: 170px;
+    text-align: center;
+    display: block;
+    margin: 25px auto;
+  }
+      
+  section {
+    padding: 30px;
+  }
+      
+  section h2 {
+    color: #333;  
+  }
+  
+  .galeria  {
+    text-align: center;   
+  }
+  
+  .galeria img {
+    width: 200px;
+    margin: 10px;
+    border-radius: 10px;
+    border: 5px solid #333;
+  }
+      
+  footer {
+    background-color: #eee;
+    padding: 10px;
+    text-align: center;
+  }
+  
